d048bf1013e7147c9466f56cd844b46a6dce52a5
Ayush Srivastava <iayush.srivastava99@gmail.com>
2021-01-07 13:41:27 +0530

    form and filter html added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
+import React,{useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+import Todolist from './components/Todolist';
+
+
+
 
 function App() {
+  const [inputText,setInputText]=useState("");
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Ayush's Todo List {inputText}</h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <Todolist />
     </div>
   );
 }
---
+++ src/components/Todolist.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+const Todolist=()=>{
+    return (
+        <div className="todo-container">
+      <ul className="todo-list"></ul>
+    </div>
+    )
+}
+export default Todolist;
